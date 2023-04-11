31b6b0710c04c8092b40864f9d69491e6edb9b1e
Simone de Abreu <siabreu@gmail.com>
2023-04-11 15:44:59 -0300

    config react in project

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react"
+import PropTypes from "prop-types"
 class HelloWorld extends React.Component {
-  render () {
+  render() {
     return (
       <React.Fragment>
-        Greeting: {this.props.greeting}
+        Message: {this.props.greeting}
       </React.Fragment>
     );
   }
